6e8a09811a7043a08986c7e1982a866c835ce646
Nick Van Dyck <vandyck.nick@outlook.com>
2021-12-23 12:07:36 +0100

    feat: add opacity to terminal

--- config.h
+++ config.h
@@ -84,6 +84,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -111,6 +114,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+    "black",
 	"#263238", // background
 	"#ECEFF1", //foreground
 
